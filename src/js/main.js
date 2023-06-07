6e3865dc0f8a793f76fbae11f9f753f84a5a5307
alonahaletska <al.khaletskaya@gmail.com>
2023-06-07 13:15:47 +0300

    підключення модального вікна та виправлення помилок

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,5 @@ import './reviews.js';
 // Підключення свайперу
 import './our-products-swiper.js';
 import 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js';
+// Підключення модального вікна subscribe
+import './subscribe-modal.js'
